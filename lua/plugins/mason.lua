a0468090adf26cdd86102162b6d47613d635199d
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-01-02 16:25:55 +0900

    fix 破壊されたので作り直し

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,9 +8,8 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "terraformls",
         "lua_ls",
         -- php
@@ -26,16 +25,15 @@ return {
         "marksman",
         -- rust
         "rust_analyzer",
-      })
-    end,
+      },
+    },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "prettier",
         -- php
         "php-cs-fixer",
@@ -50,19 +48,19 @@ return {
         "latexindent",
         -- ts
         "biome",
-      })
-    end,
+        -- add more arguments for adding more null-ls sources
+      },
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "python",
         "php",
         -- add more arguments for adding more debuggers
-      })
-    end,
+      },
+    },
   },
 }
