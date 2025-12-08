7f335438becf46b3d82867b096d22a8ee82c23a3
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-12-08 14:45:29 +0900

    fix copilot無効化

--- lua/community.lua
+++ lua/community.lua
@@ -53,20 +53,20 @@ return {
   },
 
   -- github copilot
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { -- further customize the options set by the community
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { -- further customize the options set by the community
+  --   "copilot.lua",
+  --   opts = {
+  --     suggestion = {
+  --       keymap = {
+  --         accept = "<C-l>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<C-.>",
+  --         prev = "<C-,>",
+  --         dismiss = "<C/>",
+  --       },
+  --     },
+  --   },
+  -- },
 }
